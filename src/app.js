760edaa41a537e2c0aa2f8a5c13e8fdfc068f046
grigore_v <vieru.grigore896@gmail.com>
2022-03-27 22:02:55 +0300

    update

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-
 const path = require('path');
-const PORT = process.env.PORT || 3000;
 
-app.use(express.static("assets"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.listen(PORT,()=>{
-    console.log("App was started at port: " + PORT)
+app.listen(process.env.PORT || 3000, function () {
+    let date = new Date(Date.now());
+    let serverStartTime = date.getDate() + "." + date.getMonth() + "." + date.getFullYear() + "  " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    console.log("Server was start/restart on port 3000: " + serverStartTime);
 });
